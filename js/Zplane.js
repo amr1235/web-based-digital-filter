b2e5830de2e4331ac58fc4475a071e52b09db9f9
amr mohamed <amr.ali99@eng-st.cu.edu.eg>
2021-06-05 17:26:18 +0200

    add real pole and add real zero done

--- js/Zplane.js
+++ js/Zplane.js
@@ -1,10 +1,9 @@
 // import '/digital-filter-design/d3/d3'
 class Zplane {
     constructor(width, height) {
-
         //equation x**2 + y**2 = 1
-        this.w = width
-        this.h = height
+        this.w = width;
+        this.h = height;
         this.padding = 30 ;
         this.dataSet = [];
 
@@ -15,35 +14,14 @@ class Zplane {
             .append("svg")
             .attr("width", width)
             .attr("height", height)
-            .on("click",(ev) => {
-                let xElement = ev.layerX;
-                let yElement = ev.layerY;
-                console.log(xElement,yElement)
-                let xPlane ;
-                let yPlane ;
-                if ( xElement > this.middlePointOf_x) {
-                    xPlane = this.xPositiveScale.invert(xElement);
-                } else {
-                    xPlane = this.xNegativScale.invert(xElement);
-                }
-                if ( yElement < this.middlePointOf_y) {
-                    yPlane = this.yPositiveScale.invert(yElement);
-                } else {
-                    console.log("--")
-                }
-                if(this.isInside(xElement,yElement)){
-                    console.log("inside")
-                }else{
-                    console.log("outside")
-                }
-            });
+            .attr("id","zPlane");
 
         // set our scale
         // scale x
         this.xNegativScale = d3.scaleLinear().domain([-1, 0]).range([this.padding, this.middlePointOf_x]);
         this.xPositiveScale = d3.scaleLinear().domain([0, 1]).range([this.middlePointOf_x, this.w - this.padding]);
         //scale y 
-        this.yNegativScale = d3.scaleLinear().domain([-1, 0]).range([this.middlePointOf_y, this.h - this.padding]);
+        this.yNegativScale = d3.scaleLinear().domain([0, -1]).range([this.middlePointOf_y, this.h - this.padding]);
         this.yPositiveScale = d3.scaleLinear().domain([0, 1]).range([this.middlePointOf_y, this.padding]);
 
         // set up x axis
@@ -60,10 +38,21 @@ class Zplane {
             .attr("fill", "none");
 
         this.types = {
-            pole : "pole",
-            zero : "zero"
+            realPole : "realPole",
+            realZero : "realZero"
         };
     }
+
+    //check if a point is inside the z-plane
+    isInside = (xElement,yElement) => {
+        let firstPart = (xElement - this.middlePointOf_x)*(xElement - this.middlePointOf_x);
+        let secondPart = (yElement - this.middlePointOf_y)*( yElement - this.middlePointOf_y);
+        let thirdPart = (this.middlePointOf_x - this.padding)*(this.middlePointOf_x - this.padding);
+        let ch = firstPart + secondPart - thirdPart;
+        if(ch > 1) return false;
+        return true;
+    }
+
     plot_axis(){
         //plot x
         this.svg.append("g").call(this.xPositiveAxis).attr("class", "axis")
@@ -76,23 +65,16 @@ class Zplane {
         this.svg.append("g").call(this.yNegativeAxis).attr("class", "axis")
             .attr("transform", "translate(" + this.middlePointOf_x + ",0)");
     }
-    //check if a point is inside the z-plane
-    isInside(xElement,yElement){
-        let firstPart = (xElement - this.middlePointOf_x)*(xElement - this.middlePointOf_x);
-        let secondPart = (yElement - this.middlePointOf_y)*( yElement - this.middlePointOf_y);
-        let thirdPart = (this.middlePointOf_x - this.padding)*(this.middlePointOf_x - this.padding);
-        let ch = firstPart + secondPart - thirdPart
-        if(ch > 1) return false;
-        return true;
-    }
-    add_point(point,type){
+    add_point = (point,type) => {
+        //check if the point exist 
+        //if(!this.isInside())
         // plot
         this.dataSet.push({
             point : point,
             type : type
         });
         let points = this.svg.selectAll(".point").data(this.dataSet);
-        if(type == this.types.pole) {
+        if(type == this.types.realPole) {
             points.enter().append("text").text("X").attr("x",(d) => {
                 let v = 0;
                 if (d["point"][0] > 0) {
@@ -119,9 +101,46 @@ class Zplane {
             .on("mouseout",function(ev){
                 d3.select(ev.target).attr("fill","green");
             })
+            .on("mousedown",(ev) => {
+                this.isMouseDown = true;
+                this.downElement = ev.target;
+            })
+            .on("drag",(ev) => {
+                // this.isMouseDown = false;
+                // this.downElement = undefined;
+                console.log("moving");
+            })
+            .call(d3.drag()
+                    .on("drag",(ev)=>{
+                        // get value in dataSet
+                        let point = ev.subject.point;
+                        //change value in current label
+                        d3.select("#current_x").text(point[0]);
+                        d3.select("#current_j").text(point[1]);
+                        let pIndex = -1;
+                        // get point index
+                        for (let i = 0; i < this.dataSet.length; i++) {
+                            if(this.dataSet[i]["point"] === point) {
+                                pIndex = i;
+                                break;
+                            }
+                        }
+                        //change value in dataset
+                        let xElement = ev.x;
+                        let yElement = ev.y;
+                        let mPoint = this.mapFromElementToPlaneCor(xElement,yElement);
+                        // boundry conditions
+                        if(mPoint[0] < -1) mPoint[0] = -1;
+                        if(mPoint[0] > 1) mPoint[0] = 1;
+                        this.dataSet[pIndex].point = [mPoint[0],0];
+                        // update position on plan
+                        this.update_point(this.dataSet[pIndex].type);
+                    })
+                )
+            .attr("id",this.dataSet.length - 1)
             .attr("style","cursor: default;")
             .merge(points);
-        }else {
+        }else if(type == this.types.realZero) {
             points.enter().append("circle").attr("cx",(d) => {
                 let v = 0;
                 if (d["point"][0] > 0) {
@@ -148,7 +167,75 @@ class Zplane {
             .on("mouseout",function(ev){
                 d3.select(ev.target).attr("fill","none");
             })
+            .call(d3.drag()
+                    .on("drag",(ev)=>{
+                        // get value in dataSet
+                        let point = ev.subject.point;
+                        //change value in current label
+                        d3.select("#current_x").text(point[0]);
+                        d3.select("#current_j").text(point[1]);
+                        let pIndex = -1;
+                        // get point index
+                        for (let i = 0; i < this.dataSet.length; i++) {
+                            if(this.dataSet[i]["point"] === point) {
+                                pIndex = i;
+                                break;
+                            }
+                        }
+                        //change value in dataset
+                        let xElement = ev.x;
+                        let yElement = ev.y;
+                        let mPoint = this.mapFromElementToPlaneCor(xElement,yElement);
+                        // boundry conditions
+                        if(mPoint[0] < -1) mPoint[0] = -1;
+                        if(mPoint[0] > 1) mPoint[0] = 1;
+                        this.dataSet[pIndex].point = [mPoint[0],0];
+                        // update position on plan
+                        this.update_point(this.dataSet[pIndex].type);
+                    })
+                )
+            .attr("index",this.dataSet.length - 1)
             .merge(points);
+        }
+    }
+
+    mapFromElementToPlaneCor = (xElement,yElement) => {
+        let xPlan = undefined;
+        let yPlane = undefined;
+        if (xElement > this.middlePointOf_x) {
+            xPlan = this.xPositiveScale.invert(xElement);
+        } else {
+            xPlan = this.xNegativScale.invert(xElement);
+        }
+        if (yElement <= this.middlePointOf_y) {
+            yPlane = this.yPositiveScale.invert(yElement);
+        } else {
+            yPlane = this.yNegativScale.invert(yElement);
+        }
+        return [xPlan,yPlane];
+    }
+
+    update_point = (type) => {
+        if(type == this.types.realPole) {
+            this.svg.selectAll(".point").data(this.dataSet).attr("x",(d) => {
+                let v = 0;
+                if (d["point"][0] > 0) {
+                    v = this.xPositiveScale(d["point"][0]) - 4;
+                } else {
+                    v = this.xNegativScale(d["point"][0]) - 4;
+                }
+                return v;
+            });
+        }else if(type == this.types.realZero) {
+            this.svg.selectAll(".point").data(this.dataSet).attr("cx",(d) => {
+                let v = 0;
+                if (d["point"][0] > 0) {
+                    v = this.xPositiveScale(d["point"][0]);
+                } else {
+                    v = this.xNegativScale(d["point"][0]);
+                }
+                return v;
+            });
         }
     }
 }
